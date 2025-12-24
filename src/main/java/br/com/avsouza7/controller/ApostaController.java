bbcde780017997a3183ed61cab0be24cae3ee40a
Alexandre Vieira de Souza <avsouza@softplan.com.br>
2025-12-24 17:23:56 -0300

    relatório de apostas

--- src/main/java/br/com/avsouza7/controller/ApostaController.java
+++ src/main/java/br/com/avsouza7/controller/ApostaController.java
@@ -1,17 +1,28 @@
 package br.com.avsouza7.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.avsouza7.filter.ResultadoFilter;
+import br.com.avsouza7.model.Aposta;
 import br.com.avsouza7.model.ApostadorDTO;
 import br.com.avsouza7.model.CadastroAposta;
+import br.com.avsouza7.model.Dezena;
+import br.com.avsouza7.provider.ApostaProvider;
 import br.com.avsouza7.repository.ApostaRepository;
+import br.com.avsouza7.repository.ApostadorRepository;
 import br.com.avsouza7.repository.PessoaRepository;
 import br.com.avsouza7.service.CadastroApostaService;
 
@@ -19,48 +30,77 @@ import br.com.avsouza7.service.CadastroApostaService;
 @RequestMapping("/apostas")
 public class ApostaController {
 
-  @Autowired
-  private ApostaRepository apostaRepository;
-  @Autowired
-  private CadastroApostaService cadastroApostaService;
-  @Autowired
-  private PessoaRepository pessoaRepository;
+    @Autowired
+    private ApostaRepository apostaRepository;
+    @Autowired
+    private CadastroApostaService cadastroApostaService;
+    @Autowired
+    private PessoaRepository pessoaRepository;
+    @Autowired
+    private ApostadorRepository apostadorRepository;
+    @Autowired
+    private ApostaProvider apostaProvider;
 
-  @GetMapping("/listar")
-  public ModelAndView listar() {
-    ModelAndView mv = new ModelAndView("apostas/listar");
-    mv.addObject("apostas", apostaRepository.findAll(Sort.by(Sort.Direction.DESC, "dtSorteio")));
-    return mv;
-  }
+    @GetMapping("/listar")
+    public ModelAndView listar() {
+	ModelAndView mv = new ModelAndView("apostas/listar");
+	mv.addObject("apostas", apostaRepository.findAll(Sort.by(Sort.Direction.DESC, "dtSorteio")));
+	return mv;
+    }
 
-  @GetMapping("/novo")
-  public ModelAndView novo() {
-    ModelAndView mv = new ModelAndView("apostas/cadastro");
-    CadastroAposta cadastro = new CadastroAposta();
-    cadastro.getDezenas().add("");
-    pessoaRepository.findAll(Sort.by(Sort.Direction.ASC, "nome"))
-        .forEach(p -> cadastro.getApostadores().add(new ApostadorDTO(p)));
-    mv.addObject("aposta", cadastro);
-    return mv;
-  }
+    @GetMapping("/novo")
+    public ModelAndView novo() {
+	ModelAndView mv = new ModelAndView("apostas/cadastro");
+	CadastroAposta cadastro = new CadastroAposta();
+	cadastro.getDezenas().add("");
+	pessoaRepository.findAll(Sort.by(Sort.Direction.ASC, "nome"))
+		.forEach(p -> cadastro.getApostadores().add(new ApostadorDTO(p)));
+	mv.addObject("aposta", cadastro);
+	return mv;
+    }
 
-  @GetMapping("/{id}/editar")
-  public ModelAndView editar(@PathVariable Long id) {
-    ModelAndView mv = new ModelAndView("apostas/cadastro");
-    mv.addObject("aposta", cadastroApostaService.findById(id).orElseThrow());
-    return mv;
-  }
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+	ModelAndView mv = new ModelAndView("apostas/cadastro");
+	mv.addObject("aposta", cadastroApostaService.findById(id).orElseThrow());
+	return mv;
+    }
 
-  @PostMapping("/salvar")
-  public String salvar(@ModelAttribute CadastroAposta aposta) {
-    cadastroApostaService.save(aposta);
-    return "redirect:/apostas/listar";
-  }
+    @PostMapping("/salvar")
+    public String salvar(@ModelAttribute CadastroAposta aposta) {
+	cadastroApostaService.save(aposta);
+	return "redirect:/apostas/listar";
+    }
 
-  @GetMapping("/{id}/excluir")
-  public String excluir(@PathVariable Long id) {
-    apostaRepository.deleteById(id);
-    return "redirect:/apostas/listar";
-  }
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+	apostaRepository.deleteById(id);
+	return "redirect:/apostas/listar";
+    }
+
+    @GetMapping("/apostas")
+    public ModelAndView consultar() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.addObject("resultadoFilter", new ResultadoFilter());
+	modelAndView.setViewName("apostas/apostas");
+	return modelAndView;
+    }
+
+    @PostMapping("/apostas")
+    public ModelAndView apostas(@Valid ResultadoFilter filter, BindingResult result) {
+	ModelAndView mv = new ModelAndView("apostas/apostas");
+	List<Aposta> apostas = apostaProvider.getApostas(filter);
+	apostas.forEach(aposta -> {
+	    String[] split = aposta.getDezenasApostadas().split(";");
+	    for (int i = 0; i < split.length; i++) {
+		String string = split[i];
+		aposta.getDezenas().add(new Dezena(string));
+	    }
+	});
+	mv.addObject("apostas", apostas);
+	mv.addObject("apostadores", apostadorRepository.findByIdLoteriaAndIdGrupoAndIdConcurso(filter.getIdLoteria(),
+		filter.getIdGrupo(), filter.getIdConcurso()));
+	return mv;
+    }
 
 }
--- src/main/java/br/com/avsouza7/model/Apostador.java
+++ src/main/java/br/com/avsouza7/model/Apostador.java
@@ -1,6 +1,7 @@
 package br.com.avsouza7.model;
 
 import java.math.BigDecimal;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -9,67 +10,71 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import br.com.avsouza7.util.FormataMonetario;
+
 @Entity
 public class Apostador {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long idApostador;
-  private Long idLoteria;
-  private Long idConcurso;
-  private Long idGrupo;
-  private BigDecimal aporte;
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "idPessoa")
-  private Pessoa pessoa;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idApostador;
+    private Long idLoteria;
+    private Long idConcurso;
+    private Long idGrupo;
+    private BigDecimal aporte;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idPessoa")
+    private Pessoa pessoa;
 
-  public Long getIdApostador() {
-    return idApostador;
-  }
+    public Long getIdApostador() {
+	return idApostador;
+    }
 
-  public void setIdApostador(Long idApostador) {
-    this.idApostador = idApostador;
-  }
+    public void setIdApostador(Long idApostador) {
+	this.idApostador = idApostador;
+    }
 
-  public Long getIdLoteria() {
-    return idLoteria;
-  }
+    public Long getIdLoteria() {
+	return idLoteria;
+    }
 
-  public void setIdLoteria(Long idLoteria) {
-    this.idLoteria = idLoteria;
-  }
+    public void setIdLoteria(Long idLoteria) {
+	this.idLoteria = idLoteria;
+    }
 
-  public Long getIdConcurso() {
-    return idConcurso;
-  }
+    public Long getIdConcurso() {
+	return idConcurso;
+    }
 
-  public void setIdConcurso(Long idConcurso) {
-    this.idConcurso = idConcurso;
-  }
+    public void setIdConcurso(Long idConcurso) {
+	this.idConcurso = idConcurso;
+    }
 
-  public Long getIdGrupo() {
-    return idGrupo;
-  }
+    public Long getIdGrupo() {
+	return idGrupo;
+    }
 
-  public void setIdGrupo(Long idGrupo) {
-    this.idGrupo = idGrupo;
-  }
+    public void setIdGrupo(Long idGrupo) {
+	this.idGrupo = idGrupo;
+    }
 
-  public BigDecimal getAporte() {
-    return aporte;
-  }
+    public BigDecimal getAporte() {
+	return aporte;
+    }
 
-  public void setAporte(BigDecimal aporte) {
-    this.aporte = aporte;
-  }
+    public void setAporte(BigDecimal aporte) {
+	this.aporte = aporte;
+    }
 
-  public Pessoa getPessoa() {
-    return pessoa;
-  }
+    public Pessoa getPessoa() {
+	return pessoa;
+    }
 
-  public void setPessoa(Pessoa pessoa) {
-    this.pessoa = pessoa;
-  }
+    public void setPessoa(Pessoa pessoa) {
+	this.pessoa = pessoa;
+    }
 
-
+    public String getValorDoAporte() {
+	return FormataMonetario.brasileiro(aporte);
+    }
 }
